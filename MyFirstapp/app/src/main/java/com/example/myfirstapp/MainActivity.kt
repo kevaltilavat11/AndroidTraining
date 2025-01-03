74c7cc2183173ff7c190679ef92c53ad93953324
keval tilavat <keval.tilavat@tatvasoft.com>
2025-01-03 14:09:05 +0530

    implemented validation in user details

--- MyFirstapp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstapp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.myfirstapp
 
+import com.example.myfirstapp.R
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.EditText
+import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,4 +24,29 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    fun validateDetails(view: View?) {
+        val username = findViewById<EditText>(R.id.username)
+        val email = findViewById<EditText>(R.id.user_email)
+        val password = findViewById<EditText>(R.id.user_password)
+        val contactNumber = findViewById<EditText>(R.id.contact_number)
+        val agePicker = findViewById<NumberPicker>(R.id.age_picker)
+        val usernameText = username.text.toString().trim { it <= ' ' }
+        val emailText = email.text.toString().trim { it <= ' ' }
+        val passwordText = password.text.toString().trim { it <= ' ' }
+        val contactNumberText = contactNumber.text.toString().trim { it <= ' ' }
+        val age = agePicker.value
+        if (usernameText.isEmpty() || emailText.isEmpty() || passwordText.isEmpty() || contactNumberText.isEmpty()) {
+            Toast.makeText(this, "All fields are required!", Toast.LENGTH_SHORT).show()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            Toast.makeText(this, "Invalid Email Address!", Toast.LENGTH_SHORT).show()
+        } else if (passwordText.length < 6) {
+            Toast.makeText(this, "Password must be at least 6 characters!", Toast.LENGTH_SHORT)
+                .show()
+        } else if (contactNumberText.length != 10) {
+            Toast.makeText(this, "Contact Number must be 10 digits!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Details are valid!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
