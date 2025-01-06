23f9e9aca7d68f1bed7f51b8171250c7be7c8277
keval tilavat <keval.tilavat@tatvasoft.com>
2025-01-06 16:20:10 +0530

    implemented user login screen

--- MyFirstapp/app/src/main/java/com/example/myfirstapp/Login.kt
+++ MyFirstapp/app/src/main/java/com/example/myfirstapp/Login.kt
@@ -5,12 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import android.graphics.Color
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+        window.statusBarColor = Color.parseColor("#FFFFFF")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- MyFirstapp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstapp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -28,33 +28,33 @@ class MainActivity : AppCompatActivity() {
         }
     }
     fun validateDetails(view: View?) {
-        val username = findViewById<EditText>(R.id.username)
-        val email = findViewById<EditText>(R.id.user_email)
-        val password = findViewById<EditText>(R.id.user_password)
-        val contactNumber = findViewById<EditText>(R.id.contact_number)
-        val agePicker = findViewById<NumberPicker>(R.id.age_picker)
-        val usernameText = username.text.toString().trim { it <= ' ' }
-        val emailText = email.text.toString().trim { it <= ' ' }
-        val passwordText = password.text.toString().trim { it <= ' ' }
-        val contactNumberText = contactNumber.text.toString().trim { it <= ' ' }
-        val age = agePicker.value
-        if (usernameText.isEmpty() && emailText.isEmpty() && contactNumberText.isEmpty() && contactNumberText.isEmpty()) {
-            username.setError(getString(R.string.valid_fields));
-            email.setError(getString(R.string.valid_fields))
-            password.setError(getString(R.string.valid_fields))
-            contactNumber.setError(getString(R.string.valid_fields))
-        } else if (usernameText.isEmpty()) {
-            username.setError(getString(R.string.valid_username));
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
-            email.setError(getString(R.string.valid_email))
-        } else if (passwordText.length < 6) {
-            password.setError(getString(R.string.valid_password))
-        } else if (contactNumberText.length != 10) {
-            contactNumber.setError(getString(R.string.valid_contactNumber))
-        } else {
+//        val username = findViewById<EditText>(R.id.username)
+//        val email = findViewById<EditText>(R.id.user_email)
+//        val password = findViewById<EditText>(R.id.user_password)
+//        val contactNumber = findViewById<EditText>(R.id.contact_number)
+//        val agePicker = findViewById<NumberPicker>(R.id.age_picker)
+//        val usernameText = username.text.toString().trim { it <= ' ' }
+//        val emailText = email.text.toString().trim { it <= ' ' }
+//        val passwordText = password.text.toString().trim { it <= ' ' }
+//        val contactNumberText = contactNumber.text.toString().trim { it <= ' ' }
+//        val age = agePicker.value
+//        if (usernameText.isEmpty() && emailText.isEmpty() && contactNumberText.isEmpty() && contactNumberText.isEmpty()) {
+//            username.setError(getString(R.string.valid_fields));
+//            email.setError(getString(R.string.valid_fields))
+//            password.setError(getString(R.string.valid_fields))
+//            contactNumber.setError(getString(R.string.valid_fields))
+//        } else if (usernameText.isEmpty()) {
+//            username.setError(getString(R.string.valid_username));
+//        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+//            email.setError(getString(R.string.valid_email))
+//        } else if (passwordText.length < 6) {
+//            password.setError(getString(R.string.valid_password))
+//        } else if (contactNumberText.length != 10) {
+//            contactNumber.setError(getString(R.string.valid_contactNumber))
+//        } else {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
-        }
+//        }
     }
 
 }
