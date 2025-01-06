6dfa01b4948da809ae1eb0f197a5cb8ad30ac147
keval tilavat <keval.tilavat@tatvasoft.com>
2025-01-06 16:28:23 +0530

    updated user login screen

--- MyFirstapp/app/src/main/java/com/example/myfirstapp/AlignScreen.kt
+++ MyFirstapp/app/src/main/java/com/example/myfirstapp/AlignScreen.kt
@@ -1,29 +1,20 @@
 package com.example.myfirstapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.graphics.Color
-import android.view.View
 
-class Login : AppCompatActivity() {
+class AlignScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        window.statusBarColor = Color.parseColor("#FFFFFF")
+        setContentView(R.layout.activity_align_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun authorizeClick(view: View?) {
-        val intent = Intent(this, AlignScreen::class.java)
-        startActivity(intent)
     }
 }
