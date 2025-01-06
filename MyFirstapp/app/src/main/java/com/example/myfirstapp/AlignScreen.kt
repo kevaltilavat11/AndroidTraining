6e5f6373867d5c1487b5eaff86da15387d43aec7
keval tilavat <keval.tilavat@tatvasoft.com>
2025-01-06 18:04:40 +0530

    implemented alignment screen

--- MyFirstapp/app/src/main/java/com/example/myfirstapp/AlignScreen.kt
+++ MyFirstapp/app/src/main/java/com/example/myfirstapp/AlignScreen.kt
@@ -1,10 +1,15 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.Button
+import android.widget.RelativeLayout
+import androidx.core.view.isVisible
 
 class AlignScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,20 @@ class AlignScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun topClick (view:View?){
+        val bottomContainer = findViewById<RelativeLayout>(R.id.bottom_container)
+        bottomContainer.visibility = View.INVISIBLE
+    }
+    fun centerClick (view:View?){
+        val topContainer = findViewById<RelativeLayout>(R.id.top_container)
+        val bottomContainer = findViewById<RelativeLayout>(R.id.bottom_container)
+        topContainer.visibility = View.VISIBLE
+        bottomContainer.visibility = View.VISIBLE
+    }
+    fun bottomClick (view:View?){
+        val topContainer = findViewById<RelativeLayout>(R.id.top_container)
+        topContainer.visibility = View.INVISIBLE
     }
 }
