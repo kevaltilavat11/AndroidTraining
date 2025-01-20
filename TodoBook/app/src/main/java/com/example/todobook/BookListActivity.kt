7d56a47f36269c7eb9d626f5dd85ac713799e435
keval tilavat <keval.tilavat@tatvasoft.com>
2025-01-20 15:58:37 +0530

    implemented add book anf book list

--- TodoBook/app/src/main/java/com/example/todobook/BookListActivity.kt
+++ TodoBook/app/src/main/java/com/example/todobook/BookListActivity.kt
@@ -1,7 +1,6 @@
 package com.example.todobook
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
